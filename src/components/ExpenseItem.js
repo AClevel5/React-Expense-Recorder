ff1557197618b314ad865112c59722535c7ee723
Alex Cleveland <Alexcleveland5@gmail.com>
2023-01-23 16:54:44 -0700

    CSS for Expense Item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <h2>Expense Item</h2>;
+  return (
+    <div>
+      <div>1/17/2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>120</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
