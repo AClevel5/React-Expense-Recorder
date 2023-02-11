5bcb2a58e27610e4a7d4ec15d92a793f3ad1a886
Alex Cleveland <Alexcleveland5@gmail.com>
2023-02-10 21:31:11 -0700

    adjust component file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -31,6 +31,6 @@ function App() {
       </h2>
     </div>
   );
-}
+};
 
 export default App;
